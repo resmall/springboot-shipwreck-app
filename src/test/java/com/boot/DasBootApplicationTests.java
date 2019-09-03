5949e6873aa35a25212815419c90b602d94a3815
Tiago R. Lammers <trlammers@gmail.com>
2019-09-02 23:43:47 -0300

    Added test

--- src/test/java/com/boot/DasBootApplicationTests.java
+++ src/test/java/com/boot/DasBootApplicationTests.java
@@ -1,9 +1,12 @@
 package com.boot;
 
+import com.boot.controller.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,9 @@ public class DasBootApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		HomeController hc = new HomeController();
+		String result = hc.home();
+		assertEquals(result, "Das boot, application pluralsight");
 	}
 
 }
---
+++ src/test/java/com/boot/ShipwreckControllerTest.java
@@ -0,0 +1,43 @@
+package com.boot;
+
+
+import com.boot.controller.ShipwreckController;
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ShipwreckControllerTest {
+    // Quando quero que as dependencias dessa classe sejam mockadas
+    @InjectMocks
+    private ShipwreckController sc;
+
+    // Quando quero que o junit crie mocks das classes e não use as implementações
+    @Mock
+    private ShipwreckRepository shipwreckRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testShipwreckGet() {
+        Shipwreck sw = new Shipwreck();
+        sw.setId(1l);
+        when(shipwreckRepository.getOne(1l)).thenReturn(sw);
+
+        Shipwreck wreck = sc.get(1L);
+
+        verify(shipwreckRepository).getOne(1l);
+
+        assertEquals(1l, wreck.getId().longValue());
+    }
+}
